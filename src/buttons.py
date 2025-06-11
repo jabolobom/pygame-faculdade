8ddf8c585b6531633e14acf282058efc23882cdb
bruno <brunophilippsen266@gmail.com>
2025-06-11 15:57:46 -0300

    Mockup de um menu principal, sem funcionalidade, esta no root do projeto temporariamente

---
+++ src/buttons.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Buttons():
+    def __init__(self, pos, text_input, font, base_color, hover_color):
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, base_color)
+        self.base_color, self.hover_color = base_color, hover_color
+        self.active = False
+
+        self.rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def update(self, screen):
+        screen.blit(self.text, self.rect)
+
+    def change_color(self, new_color):
+        if self.active:
+            self.text = self.font.render(self.text_input, True, self.hover_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
--- src/settings.py
+++ src/settings.py
@@ -4,3 +4,4 @@ SCREEN_WIDTH = 11 * TILE_SIZE # Ajuste conforme o tamanho do mapa e TILE_SIZE
 SCREEN_HEIGHT = 9 * TILE_SIZE
 PREMADE_MAP_PATH = "assets/maps/premade"
 USER_MAP_PATH = "assets/maps/usermade"
+FONT_PATH = "assets/font.ttf"
